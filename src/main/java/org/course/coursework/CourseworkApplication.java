35bbd7eb6cee3e690b97a9c39ba5ca2fa93a2c27
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-05 17:36:52 +0300

    initial commit

---
+++ src/main/java/org/course/coursework/CourseworkApplication.java
@@ -0,0 +1,13 @@
+package org.course.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/course/coursework/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.course.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
