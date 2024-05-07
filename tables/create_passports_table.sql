1403e86e858b207a2e17559e874b6ba5d93a7a3c
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-07 10:16:26 +0300

    change users and passport

--- tables/create_passports_table.sql
+++ tables/create_passports_table.sql
@@ -1,9 +1,7 @@
 CREATE TABLE passports(
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER,
     last_name VARCHAR(255),
     first_name VARCHAR(255),
     second_name VARCHAR(255),
-    age INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    age INTEGER
 );
--- tables/create_users_table.sql
+++ tables/create_users_table.sql
@@ -2,5 +2,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(255),
     email VARCHAR(255),
-    password_hash VARCHAR(255)
+    password_hash VARCHAR(255),
+    passport_id INTEGER,
+    FOREIGN KEY (passport_id) REFERENCES passports(id)
 );
