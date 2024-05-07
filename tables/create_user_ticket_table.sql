a595ad4b27b72745374a9583694f685559b3c5bf
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-07 18:08:32 +0300

    Remove user_ticket_table

--- tables/create_user_ticket_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_ticket(
-    user_id INTEGER,
-    ticket_id INTEGER,
-    PRIMARY KEY (user_id, ticket_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (ticket_id) REFERENCES tickets(id)
-);
