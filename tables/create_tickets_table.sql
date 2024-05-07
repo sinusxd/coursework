7ca0e3c1ee0cca9b4794745719de1e7b2dd75619
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-07 15:26:17 +0300

    change ManyToMany to OneToMany

--- tables/create_tickets_table.sql
+++ tables/create_tickets_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE tickets(
     id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER,
     flight_id INTEGER,
     seat_number VARCHAR(255),
-    class VARCHAR(255)
+    class VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
