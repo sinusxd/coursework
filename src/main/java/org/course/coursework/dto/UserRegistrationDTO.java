c04dd1d5dc7c35fe8710edb46acedc3882be8cf5
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-09 19:42:22 +0300

    Add DTO for registration

---
+++ src/main/java/org/course/coursework/dto/UserRegistrationDTO.java
@@ -0,0 +1,76 @@
+package org.course.coursework.dto;
+
+public class UserRegistrationDTO {
+    String username;
+    String email;
+    String lastName;
+    String firstName;
+    String secondName;
+    Integer age;
+    String password;
+    String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
