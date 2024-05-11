cd9fd19dd309d27b1dd2ae4486430089b4286491
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-05-11 15:01:28 +0300

    Add FlightNotFoundException

---
+++ src/main/java/org/course/coursework/exception/FlightNotFoundException.java
@@ -0,0 +1,7 @@
+package org.course.coursework.exception;
+
+public class FlightNotFoundException extends Exception{
+    public FlightNotFoundException(String message){
+        super(message);
+    }
+}
